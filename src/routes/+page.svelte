e733d1cefb1aedf5b569aacdbe01de84a2d5febc
Collin <cohammond12@gmail.com>
2026-04-06 02:44:37 -0500

    Added icons, made app bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
     import { jobs, punches } from "./state.svelte";
+    import Menu from "@lucide/svelte/icons/menu";
+    import Settings from "@lucide/svelte/icons/settings";
 </script>
 
 <main class="h-screen">
     <div
-        class="w-full flex bg-blue-600 justify-between text-white p-3 min-h-18 items-center"
+        class="w-full flex bg-blue-600 justify-between text-white px-2 py-3 min-h-18 items-center"
     >
-        <h1 class="text-xl font-bold">Timestamp</h1>
+        <div class="flex items-center gap-0.5">
+            <button
+                class="btn btn-circle border-none bg-none hover:bg-white/20 h-10 w-10"
+            >
+                <Menu />
+            </button>
+            <button
+                class="btn border-none bg-none hover:bg-white/20 px-2 rounded-md"
+            >
+                <h1 class="text-[1.3rem] font-bold">Timestamp</h1>
+            </button>
+        </div>
+        <div>
+            <button
+                class="btn btn-circle border-none bg-none hover:bg-white/20 h-10 w-10"
+            >
+                <Settings />
+            </button>
+        </div>
     </div>
 </main>
